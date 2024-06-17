87754c0b584130eb02e25fe3bdfc75aadea34980
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2024-06-17 19:39:00 -0300

    build: create new project

---
+++ fase-2/new-parquimetro/src/main/java/br/com/fiap/newparquimetro/NewParquimetroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.newparquimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewParquimetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewParquimetroApplication.class, args);
+	}
+
+}
---
+++ fase-2/new-parquimetro/src/test/java/br/com/fiap/newparquimetro/NewParquimetroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.newparquimetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewParquimetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
