1ebc1531c8aee0b8214c4d78293087fd7dc64ad0
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2024-06-29 19:17:52 -0300

    feat: adiciona o metodo buscar forma de pagamento

--- fase-2/new-parquimetro/src/main/java/br/com/fiap/newparquimetro/dto/FormaPagamentoResponseDTO.java
+++ fase-2/new-parquimetro/src/main/java/br/com/fiap/newparquimetro/dto/FormaPagamentoResponseDTO.java
@@ -1,6 +1,6 @@
 package br.com.fiap.newparquimetro.dto;
 
-import br.com.fiap.newparquimetro.domain.condutor.FormaPagamento;
+import br.com.fiap.newparquimetro.domain.formapagamento.FormaPagamento;
 
 public record FormaPagamentoResponseDTO(String id, String tipoDescricao) {
     public static FormaPagamentoResponseDTO toDTO(FormaPagamento formaPagamento) {
--- fase-2/new-parquimetro/src/main/java/br/com/fiap/newparquimetro/service/FormaPagamentoService.java
+++ fase-2/new-parquimetro/src/main/java/br/com/fiap/newparquimetro/service/FormaPagamentoService.java
@@ -1,6 +1,6 @@
 package br.com.fiap.newparquimetro.service;
 
-import br.com.fiap.newparquimetro.domain.condutor.FormaPagamento;
+import br.com.fiap.newparquimetro.domain.formapagamento.FormaPagamento;
 import br.com.fiap.newparquimetro.dto.FormaPagamentoResponseDTO;
 import br.com.fiap.newparquimetro.repositories.FormaPagamentoRepository;
 import lombok.AllArgsConstructor;
