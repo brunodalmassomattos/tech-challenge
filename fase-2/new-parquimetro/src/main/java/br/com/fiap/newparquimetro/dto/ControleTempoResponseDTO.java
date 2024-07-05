3424ab86b1661b1513ed2b213d7dba2df5c9f6c2
guilherme07092 <guibazzo50@gmail.com>
2024-07-05 16:15:11 -0300

    feat: adiciona funcionalidade de controle de tempo

---
+++ fase-2/new-parquimetro/src/main/java/br/com/fiap/newparquimetro/dto/ControleTempoResponseDTO.java
@@ -0,0 +1,40 @@
+package br.com.fiap.newparquimetro.dto;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.fiap.newparquimetro.domain.condutor.Tempo;
+
+public record ControleTempoResponseDTO(String id,String Data,LocalTime hrInicio,LocalTime hrFim,String idCondutor,String status){
+	
+    public static String parseDate(Date data) {
+        if (data == null) {
+            return null;
+        }
+
+        return new SimpleDateFormat("yyyy-MM-dd").format(data);
+    }
+    
+    public static ControleTempoResponseDTO toDto(Tempo tempo) {
+    	
+    	return new ControleTempoResponseDTO(
+    			tempo.getId(), 
+    			ControleTempoResponseDTO.parseDate(tempo.getData()),
+    			tempo.getHrInicio(),
+    			tempo.getHrFim(),
+    			tempo.getIdCondutor(),
+    			tempo.getStatus());
+    	
+    }
+    
+
+    public static List<ControleTempoResponseDTO> toDtoList(List<Tempo> tempos) {
+        return tempos.stream()
+                     .map(ControleTempoResponseDTO::toDto)
+                     .collect(Collectors.toList());
+    }
+    
+}
---
+++ fase-2/new-parquimetro/src/main/java/br/com/fiap/newparquimetro/service/ControleTempoService.java
@@ -0,0 +1,64 @@
+package br.com.fiap.newparquimetro.service;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.newparquimetro.controller.exception.ControllerNotFoundException;
+import br.com.fiap.newparquimetro.domain.condutor.Tempo;
+import br.com.fiap.newparquimetro.dto.ControleTempoResponseDTO;
+import br.com.fiap.newparquimetro.repositories.CondutorRepository;
+import br.com.fiap.newparquimetro.repositories.ControleTempoRepository;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class ControleTempoService {
+
+    private final ControleTempoRepository controleTempoRepository;
+    
+    private final CondutorRepository condutorRepository;
+        
+    public List<ControleTempoResponseDTO> buscaTempo(String idCondutor, String status) {
+    	
+    	return ControleTempoResponseDTO.toDtoList(this.controleTempoRepository.findTempoAtivoCondutorStatus(idCondutor,status));
+    	
+    }
+    
+    public ControleTempoResponseDTO save(String idCondutor, Long tempoContratado) {
+
+    	this.condutorRepository.findById(idCondutor).orElseThrow(() -> new ControllerNotFoundException("Condutor não encontrado"));
+    	
+    	Tempo tempo = new Tempo();
+    	
+        LocalTime horaAtual = LocalTime.now();
+        LocalDate dataAtual = LocalDate.now();
+        
+        tempo.setData(Date.from(dataAtual.atStartOfDay(ZoneId.systemDefault()).toInstant()));
+        tempo.setHrInicio(horaAtual);
+        tempo.setHrFim(horaAtual.plusMinutes(tempoContratado));
+        tempo.setIdCondutor(idCondutor);
+        tempo.setStatus("ABERTO");
+        
+        return ControleTempoResponseDTO.toDto(this.controleTempoRepository.save(tempo));
+
+    }
+    
+    public ControleTempoResponseDTO update(String idCondutor, Long tempoContratado) {
+        
+    	this.condutorRepository.findById(idCondutor).orElseThrow(() -> new ControllerNotFoundException("Condutor não encontrado"));
+    	
+    	Tempo tempo = this.controleTempoRepository.findTempoAtivoCondutorStatus(idCondutor,"ABERTO").get(0);
+    	
+    	tempo.setHrFim(tempo.getHrFim().plusMinutes(tempoContratado));
+
+    	return ControleTempoResponseDTO.toDto(this.controleTempoRepository.save(tempo));
+    	
+    }
+    
+	
+}
