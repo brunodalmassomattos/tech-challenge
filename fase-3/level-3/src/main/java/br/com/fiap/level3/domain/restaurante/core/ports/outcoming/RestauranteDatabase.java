30c94f1c3c17e5f308262499fa128185e2dbc4a9
WendelSReis <wendel.m.w18@gmail.com>
2024-09-15 12:37:54 -0300

    feat: Ajuste database e Adapter

--- fase-3/level-3/src/main/java/br/com/fiap/level3/domain/restaurante/core/ports/outcoming/RestauranteDatabase.java
+++ fase-3/level-3/src/main/java/br/com/fiap/level3/domain/restaurante/core/ports/outcoming/RestauranteDatabase.java
@@ -1,5 +1,6 @@
 package br.com.fiap.level3.domain.restaurante.core.ports.outcoming;
 
+import br.com.fiap.level3.domain.restaurante.core.domain.model.endereco.Endereco;
 import br.com.fiap.level3.domain.restaurante.core.domain.model.restaurante.Restaurante;
 
 import java.util.List;
@@ -8,9 +9,11 @@ import java.util.UUID;
 
 public interface RestauranteDatabase {
     Optional<Restaurante> getRestauranteById(UUID id);
+    Optional<Endereco> getEnderecoById(UUID id);
     List<Restaurante> getRestauranteByNome(String nome);
     List<Restaurante> getRestaurantes();
 
     void save(Restaurante restaurante);
     void update(Restaurante restaurante);
+    void updateEndereco(Endereco endereco);
 }
--- fase-3/level-3/src/main/java/br/com/fiap/level3/domain/restaurante/infrastructure/RestauranteDatabaseAdapter.java
+++ fase-3/level-3/src/main/java/br/com/fiap/level3/domain/restaurante/infrastructure/RestauranteDatabaseAdapter.java
@@ -1,5 +1,6 @@
 package br.com.fiap.level3.domain.restaurante.infrastructure;
 
+import br.com.fiap.level3.domain.restaurante.core.domain.model.endereco.Endereco;
 import br.com.fiap.level3.domain.restaurante.core.domain.model.restaurante.Restaurante;
 import br.com.fiap.level3.domain.restaurante.core.ports.outcoming.RestauranteDatabase;
 import br.com.fiap.level3.domain.restaurante.infrastructure.mapper.RestauranteRowMapper;
@@ -140,6 +141,53 @@ public class RestauranteDatabaseAdapter implements RestauranteDatabase {
                     enderecoId);
         } catch (DataAccessException e) {
             System.out.println();
+        }
+    }
+
+    @Override
+    public void updateEndereco(Endereco endereco) {
+        final String SQL_UPDATE_ENDERECO = """
+            UPDATE enderecos SET 
+                cep = ?, 
+                logradouro = ?, 
+                numero = ?, 
+                bairro = ?, 
+                cidade = ?, 
+                estado = ?
+            WHERE id = ?
+        """;
+
+        int updated = jdbcTemplate.update(
+                SQL_UPDATE_ENDERECO,
+                endereco.getCep(),
+                endereco.getLogradouro(),
+                endereco.getNumero(),
+                endereco.getBairro(),
+                endereco.getCidade(),
+                endereco.getEstado(),
+                endereco.getId()
+        );
+
+        if (updated != 1) {
+            throw new DataAccessException("Falha ao atualizar o endereço: Nenhuma linha afetada.") {};
+        }
+    }
+    @Override
+    public Optional<Endereco> getEnderecoById(UUID id) {
+        try {
+            return Optional.ofNullable(jdbcTemplate.queryForObject(
+                    "SELECT * FROM enderecos WHERE id = ?",
+                    new Object[]{id},
+                    (rs, rowNum) -> new Endereco(
+                            rs.getString("cep"),
+                            rs.getString("logradouro"),
+                            rs.getString("numero"),
+                            rs.getString("bairro"),
+                            rs.getString("cidade"),
+                            rs.getString("estado")
+                    )));
+        } catch (DataAccessException e) {
+            return Optional.empty();
         }
     }
 }
