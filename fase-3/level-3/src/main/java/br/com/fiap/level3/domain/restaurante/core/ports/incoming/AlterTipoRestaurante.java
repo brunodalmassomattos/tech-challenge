a9f620b8f93e1898e038b34e4cdbacbe642d0245
WendelSReis <wendel.m.w18@gmail.com>
2024-09-23 20:36:23 -0300

    fix: merge branch aline

--- fase-3/level-3/src/main/java/br/com/fiap/level3/domain/restaurante/core/ports/incoming/AlterTipoRestaurante.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.level3.domain.restaurante.core.ports.incoming;
-
-import br.com.fiap.level3.domain.restaurante.core.domain.model.tiporestaurante.TipoRestaurante;
-
-import java.util.UUID;
-
-public interface AlterTipoRestaurante {
-    void alterTipoRestaurante(UUID idRestaurante, TipoRestaurante tipoRestaurante);
-}
