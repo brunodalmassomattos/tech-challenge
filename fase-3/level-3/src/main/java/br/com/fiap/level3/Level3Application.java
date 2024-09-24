74b36dc7867b0bd1ed445c8feeb883e66fffb585
Guilherme Bazzo <guibazzo50@gmail.com>
2024-09-23 22:16:10 -0300

    Update Level3Application.java
    Add import AvaliacaoConfig

--- fase-3/level-3/src/main/java/br/com/fiap/level3/Level3Application.java
+++ fase-3/level-3/src/main/java/br/com/fiap/level3/Level3Application.java
@@ -2,6 +2,7 @@ package br.com.fiap.level3;
 
 import br.com.fiap.level3.infrastructure.RestauranteDomainConfig;
 import br.com.fiap.level3.infrastructure.TipoRestauranteDomainConfig;
+import br.com.fiap.level3.infrastructure.AvaliacaoConfig;
 import br.com.fiap.level3.infrastructure.ReservaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
