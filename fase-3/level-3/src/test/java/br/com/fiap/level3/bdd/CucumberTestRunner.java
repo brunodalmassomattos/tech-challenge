07a3e7b59db536a84f64646897759ade902bb64d
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2024-09-27 21:33:34 -0300

    test: adiciona testes

--- fase-3/level-3/src/test/java/br/com/fiap/level3/bdd/CucumberTestRunner.java
+++ fase-3/level-3/src/test/java/br/com/fiap/level3/bdd/CucumberTestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
+        features = "src/test/resources/features/",
         glue = {"br.com.fiap.level3.bdd"},
         plugin = {"pretty", "html:target/cucumber-reports"}
 )
