2b06c6e5dfcd25ae5f94f2fecf54074327bd70c9
WendelSReis <wendel.m.w18@gmail.com>
2024-11-26 20:38:44 -0300

    Estrutura básica

---
+++ fase-4/funcionalidadeDeCargaDeProdutos/src/main/java/br/com/fiap/funcionalidadeDeCargaDeProdutos/FuncionalidadeDeCargaDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.funcionalidadeDeCargaDeProdutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionalidadeDeCargaDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionalidadeDeCargaDeProdutosApplication.class, args);
+	}
+
+}
