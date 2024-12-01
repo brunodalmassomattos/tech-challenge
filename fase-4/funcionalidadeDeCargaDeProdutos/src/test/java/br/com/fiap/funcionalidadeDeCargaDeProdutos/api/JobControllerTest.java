78b39107f2fb3011c4f4edd56bd82ed2e6766e94
WendelSReis <wendel.m.w18@gmail.com>
2024-12-01 19:50:54 -0300

    test: testes finais e README

--- fase-4/funcionalidadeDeCargaDeProdutos/src/test/java/br/com/fiap/funcionalidadeDeCargaDeProdutos/api/JobControllerTest.java
+++ fase-4/funcionalidadeDeCargaDeProdutos/src/test/java/br/com/fiap/funcionalidadeDeCargaDeProdutos/api/JobControllerTest.java
@@ -1,4 +1,39 @@
 package br.com.fiap.funcionalidadeDeCargaDeProdutos.api;
 
+import br.com.fiap.funcionalidadeDeCargaDeProdutos.FuncionalidadeDeCargaDeProdutosApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = FuncionalidadeDeCargaDeProdutosApplication.class
+)
+@ActiveProfiles("test")
 public class JobControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void testImportJobEndpoint() {
+        String url = "http://localhost:" + port + "/jobs/import";
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        response.getBody();
+    }
 }
--- fase-4/funcionalidadeDeCargaDeProdutos/src/test/java/br/com/fiap/funcionalidadeDeCargaDeProdutos/domain/repository/ProdutoRepositoryTest.java
+++ fase-4/funcionalidadeDeCargaDeProdutos/src/test/java/br/com/fiap/funcionalidadeDeCargaDeProdutos/domain/repository/ProdutoRepositoryTest.java
@@ -1,4 +1,65 @@
 package br.com.fiap.funcionalidadeDeCargaDeProdutos.domain.repository;
 
+import br.com.fiap.funcionalidadeDeCargaDeProdutos.domain.entity.Categoria;
+import br.com.fiap.funcionalidadeDeCargaDeProdutos.domain.entity.Produto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
 public class ProdutoRepositoryTest {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Test
+    public void whenSaveAndFindProdutoTest() {
+        Categoria categoria = new Categoria();
+        categoria.setDescricao("Categoria Teste");
+        categoria = categoriaRepository.save(categoria);
+
+        Produto produto = new Produto();
+        produto.setNome("Produto Teste");
+        produto.setDescricao("Descrição do produto teste");
+        produto.setPreco(new BigDecimal("100.00"));
+        produto.setQtdEstoque(10);
+        produto.setCategoria(categoria);
+
+        produto = produtoRepository.save(produto);
+
+        Optional<Produto> found = produtoRepository.findById(produto.getId());
+
+        assertTrue(found.isPresent());
+        assertEquals(produto.getNome(), found.get().getNome());
+    }
+
+    @Test
+    public void whenFindByNomeTest() {
+        Categoria categoria = new Categoria();
+        categoria.setDescricao("Categoria Teste");
+        categoria = categoriaRepository.save(categoria);
+
+        Produto produto = new Produto();
+        produto.setNome("Produto Único");
+        produto.setDescricao("Descrição do produto único");
+        produto.setPreco(new BigDecimal("200.00"));
+        produto.setQtdEstoque(5);
+        produto.setCategoria(categoria);
+
+        produtoRepository.save(produto);
+
+        Optional<Produto> found = produtoRepository.findByNome("Produto Único");
+
+        assertTrue(found.isPresent());
+        assertEquals(produto.getDescricao(), found.get().getDescricao());
+    }
 }
+
