e7b182a6ce22917e7b38e6a353fe17d727f3f7d6
Bruno Dalmasso <bruno.mdalmasso@gmail.com>
2024-12-02 20:13:52 -0300

    test:

---
+++ fase-4/mscatalogoproduto/src/main/java/com/fiap/techchallenge/mscatalogoproduto/CatalogoProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.techchallenge.mscatalogoproduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdutosApplication.class, args);
+	}
+
+}
